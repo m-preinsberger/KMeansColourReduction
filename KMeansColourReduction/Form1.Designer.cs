aa28f80b2b4fd9bc7c2a10c6d19c524fd83f72f2
m-preinsberger <20212015@atn.ac.at>
2024-11-05 09:38:25 +0100

    Add project files.

---
+++ KMeansColourReduction/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace KMeansColourReduction
+{
+    partial class KMean
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(KMean));
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.ChoosePicBtn = new System.Windows.Forms.Button();
+            this.pictureBoxIn = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // ChoosePicBtn
+            // 
+            this.ChoosePicBtn.Location = new System.Drawing.Point(98, 198);
+            this.ChoosePicBtn.Name = "ChoosePicBtn";
+            this.ChoosePicBtn.Size = new System.Drawing.Size(197, 52);
+            this.ChoosePicBtn.TabIndex = 0;
+            this.ChoosePicBtn.Text = "Choose Picture";
+            this.ChoosePicBtn.UseVisualStyleBackColor = true;
+            this.ChoosePicBtn.Click += new System.EventHandler(this.ChoosePicBtn_Click);
+            // 
+            // pictureBoxIn
+            // 
+            this.pictureBoxIn.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxIn.Name = "pictureBoxIn";
+            this.pictureBoxIn.Size = new System.Drawing.Size(395, 449);
+            this.pictureBoxIn.TabIndex = 1;
+            this.pictureBoxIn.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(402, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(395, 449);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // KMean
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.ChoosePicBtn);
+            this.Controls.Add(this.pictureBoxIn);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "KMean";
+            this.Text = "KMean";
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button ChoosePicBtn;
+        private System.Windows.Forms.PictureBox pictureBoxIn;
+    }
+}
+
---
+++ KMeansColourReduction/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KMeansColourReduction
+{
+    public partial class KMean : Form
+    {
+        public KMean()
+        {
+            InitializeComponent();
+        }
+
+        private void ChoosePicBtn_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = openFileDialog.FileName;
+                    // The image should be displayed in the picture box
+                    Image image = Image.FromFile(filePath);
+                    pictureBoxIn.Image = image;
+                    // the imaage should be displayed accuratly not only a corner of the image
+                    pictureBoxIn.SizeMode = PictureBoxSizeMode.StretchImage;
+                    // TODO now we need to call the actual function that reduces the picture
+                }
+            }
+        }
+
+        private void ReducePictureKmeans(Image image)
+        {
+            // TODO get the image and call help functions
+            Bitmap bitmap = new Bitmap(image);
+            var pixels = GetPixels(bitmap);
+
+            // TODO Step 1 create random centroids
+            
+           
+            // TODO Step 2 every pixel is grouped to the nearest Zentroid
+
+            // TODO Step 3 Move the centroid in the new centre of gravity
+            
+            // TODO Step 4 group the pixels freshly since the centroid moved
+
+            // TODO Repeat step 3 and 4 until there are no differences anymore
+        }
+
+        /// <summary>
+        /// Extracts the color of each pixel from the given bitmap and returns them as a list of Color objects.
+        /// </summary>
+        /// <param name="bitmap">The bitmap from which to extract pixel colors.</param>
+        /// <returns>A list of Color objects representing the colors of each pixel in the bitmap.</returns>
+        private List<Color> GetPixels(Bitmap bitmap)
+        {
+            List<Color> pixels = new List<Color>();
+            for (int y = 0; y < bitmap.Height; y++)
+            {
+                for (int x = 0; x < bitmap.Width; x++)
+                {
+                    Color pixelColor = bitmap.GetPixel(x, y);
+                    pixels.Add(pixelColor);
+                }
+            }
+            return pixels;
+        }
+    }
+}
